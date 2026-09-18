90388a861c6309f08f7897c20f49a49099535211
unknown <0520fabian@gmail.com>
2026-09-18 11:12:20 +0200

    Subida de proyecto

---
+++ Prueba_Nomina/src/main/java/nominas/laboral/Persona.java
@@ -0,0 +1,40 @@
+package nominas.laboral;
+
+/**
+ * Representa una persona dentro del mundo laboral
+ */
+public class Persona {
+    /**
+     *
+     */
+    public String nombre;
+    public String dni;
+    public char sexo;
+
+    public Persona(String nombre, String dni, char sexo) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.sexo = sexo;
+    }
+
+    public Persona(String nombre, char sexo) {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", dni='" + dni + '\'' +
+                ", sexo=" + sexo +
+                '}';
+    }
+
+
+
+}
